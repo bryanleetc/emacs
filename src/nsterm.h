b3dd80ca4300378d7c374376eaa3e1f92a13d009
Alan Third <alan@idiocy.org>
2020-12-19 20:46:55 +0000

    Fix GNUstep warnings
    * src/nsterm.h: EmacsSurface is only required if NS_DRAW_TO_BUFFER is
    defined.
    * src/nsterm.m (ns_judge_scroll_bars): Remove unused variable.
    * src/nsmenu.m (update_frame_tool_bar):
    (ns_update_menubar): Remove unused variables.

--- src/nsterm.h
+++ src/nsterm.h
@@ -706,7 +706,7 @@ typedef id instancetype;
 + (CGFloat)scrollerWidth;
 @end
 
-
+#ifdef NS_DRAW_TO_BUFFER
 @interface EmacsSurface : NSObject
 {
   NSMutableArray *cache;
@@ -723,6 +723,7 @@ typedef id instancetype;
 - (void) releaseContext;
 - (IOSurfaceRef) getSurface;
 @end
+#endif
 
 
 /* ==========================================================================
